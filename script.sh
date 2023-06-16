65d8d064e4ef1f7501f246184bef8c0f7411d977
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-06-15 21:18:34 -0400

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ script.sh
@@ -0,0 +1,10 @@
+sudo apt remove mkisofs
+sudo apt remove genisoimage
+
+sudo apt install gcc g++ -y
+wget -L https://sourceforge.net/projects/schilytools/files/schily-2021-09-18.tar.bz2
+
+tar -xvf schily-2021-09-18.tar.bz2
+cd schily-2021-09-18
+sudo make
+sudo make install
