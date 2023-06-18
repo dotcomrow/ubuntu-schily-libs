734e15912a7414765867542fcf767ffa1340f540
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-06-18 09:13:00 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -16,6 +16,8 @@ sudo cp mkisofs/OBJ/x86_64-linux-gcc/mkisofs /usr/bin/mkisofs
 mkisofs --version
 
 fallocate -l 6G test.img 
-
+wget -L https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
+unzip syslinux-6.03.zip
+cp bios/core/isolinux.bin .
 mkisofs -o test.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 3 -J -R -V "test" .
 
