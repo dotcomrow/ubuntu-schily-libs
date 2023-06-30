02e10d9f244e425cc1f10e159d1436ab9200c043
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-06-29 20:27:19 -0400

    test update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- script.sh
+++ script.sh
@@ -1,24 +1,25 @@
 sudo apt remove mkisofs
 sudo apt remove genisoimage
 
-sudo apt install gcc g++ syslinux-utils -y
-isohybrid --version
-wget -L https://sourceforge.net/projects/schilytools/files/schily-2021-09-18.tar.bz2
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install gcc g++ syslinux-utils xorriso -y
+# isohybrid --version
+# wget -L https://sourceforge.net/projects/schilytools/files/schily-2021-09-18.tar.bz2
 
-tar -xvf schily-2021-09-18.tar.bz2
-cd schily-2021-09-18
-sudo make
-sudo make install
+# tar -xvf schily-2021-09-18.tar.bz2
+# cd schily-2021-09-18
+# sudo make
+# sudo make install
 
-ls -al mkisofs/OBJ/x86_64-linux-gcc/mkisofs
+# ls -al mkisofs/OBJ/x86_64-linux-gcc/mkisofs
 
-sudo cp mkisofs/OBJ/x86_64-linux-gcc/mkisofs /usr/bin/mkisofs
-mkisofs --version
+# sudo cp mkisofs/OBJ/x86_64-linux-gcc/mkisofs /usr/bin/mkisofs
+# mkisofs --version
 
 fallocate -l 6G test.img 
 wget -L https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip
 unzip syslinux-6.03.zip
 cp bios/core/isolinux.bin .
 
-sudo mkisofs -o /test.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 3 -J -R -V "test" .
+sudo xorriso -as mkisofs -o /test.iso -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 3 -J -R -V "test" .
 
